d568bcb6ee0fa3765890151dd61eb1795c4c0c9f
FelipeMFraga <femf20795@gmail.com>
2024-12-26 19:44:21 -0300

    Adicionando userData json

--- e2e/login.spec.cy.js
+++ e2e/login.spec.cy.js
@@ -1,3 +1,5 @@
+import userData from './cypress/fixtures/userData.json'
+
 describe('Orange HRM Teste ', () => {
 
   const selectorsList = {
@@ -5,21 +7,22 @@ describe('Orange HRM Teste ', () => {
     passwordField: "[name='password']",
     loginButton: "[type='submit']",
     sectionTielTopBar: '.oxd-topbar-header-breadcrumb-module',
+    dashboardGrid: '.orangehrm-dashboard-grid',
     wrongCredentialAlert: ".oxd-alert-content-text"
   }
 
   it('Login realizado com Sucesso!', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-    cy.get(selectorsList.usernameField).type('Admin')
-    cy.get(selectorsList.passwordField).type('admin123')
+    cy.get(selectorsList.usernameField).type(userData.userSucess.login)
+    cy.get(selectorsList.passwordField).type(userData.userSucess.senha)
     cy.get(selectorsList.loginButton).click()
     cy.location('pathname').should('eq', '/web/index.php/dashboard/index')
-    cy.get(selectorsList.sectionTielTopBar).contains('Dashboard')
+    cy.get(selectorsList.dashboardGrid)
   })
   it('Login - Fail', () => {
   cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-  cy.get(selectorsList.usernameField).type('Admin')
-  cy.get(selectorsList.passwordField).type('teste')
+  cy.get(selectorsList.usernameField).type(userData.userFail.login)
+  cy.get(selectorsList.passwordField).type(userData.userFail.senha)
   cy.get(selectorsList.loginButton).click()
   cy.get(selectorsList.wrongCredentialAlert)//aqui ele identifica o alerta de credencial invalida, se por acaso logar mesmo com credenciais invalidas, ele vai apontar erro aqui no cypress, pois não vai aparecer, para ser true precisa aparecer.
   })
